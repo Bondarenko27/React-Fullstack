b6a2c3cec91bc700e43578b4635034117a5cd0ef
Bondarenko27 <bondarenkokristina27@gmail.com>
2022-01-02 12:33:57 -0500

    add comments section

--- front-end/src/pages/OpenPost.js
+++ front-end/src/pages/OpenPost.js
@@ -6,20 +6,42 @@ import axios from "axios";
 function OpenPost(){
     let{id} = useParams();
     const[postObject,setPostObject] = useState ({});
-
+    const[comments, setComments] = useState([]);
+    const[newComment, setNewComment] = useState("");
     useEffect(() => {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
             setPostObject(response.data);
-        })
+        });
+        axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+            setComments(response.data);
+        });
+        const addComment=()=>{
+            axios.post("http://localhost:3001/comments",{commentBody: newComment,PostId:id })
+            .then((response)=> {
+            console.log("comment added!");
+         } );
 
-    })
+        };
+
+    }, []);
 
     return <div className="postPage">
         <div className="leftSide"></div>
         <div className="title">{postObject.title}</div>
         <div className="text">{postObject.text}</div>
         <div className="username">{postObject.username}</div>
-        <div className="rightSide"> comments</div>
+        <div className="rightSide">
+            <div className="addCommentContainer">
+                <input type="text" placeholder="Comment.." 
+                autoComplete="off" onChange={(event) => {setNewComment(event.target.value)}}/>
+                <button onClick={addComment}>Add Comments</button>
+                </div> 
+            <div className="listOfComments">
+              {comments.map((comment,key) =>{
+                  return<div key={key} className="comment">{comment.commentBody}</div>
+              })}
+            </div>
+        </div>
         </div>;
 
 }
